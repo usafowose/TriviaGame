0d36d1a3e487825a4f472e785126e0cf076875f7
Andrew Fowose <usafowose@gmail.com>
2019-09-27 17:40:04 -0400

    All questions on html and js progress

--- Assets/JavaScript/app.js
+++ Assets/JavaScript/app.js
@@ -1,29 +1,52 @@
+var questions = {
+  firstQ: "In what year was Michael Jordan drafted?",
+  secondQ: "Who was the previous NBA Commisioner before Adam Silver?",
+  thirdQ: "Which NBA Franchise has won the most NBA Championship Titles?",
+  fourthQ:
+    "What is it called when a player fouls a bad free-throw shooter intentionally?",
+  fifthQ: "Which NBA coach has coached both Michael Jordan and Kobe Bryant?",
+  sixthQ:
+    "The Los Angeles Lakers won the 1980 NBA Championship with who as their assisstant coach?",
+  seventhQ: "Dwyane Wade's nickname is ... ?"
+};
 $(document).ready(function() {
   console.log("Ready!");
   console.log("Time for our JS and JQuery to kick in!");
 
+  var time = 60; 
+  var timerval; 
+  isCorrect = 0; 
+  inIncorrect = 0; 
+  isUnanswered = 0;  
+
   var $body = $("body");
   var $table = $("table");
-  $table.hide();
+  hideTable();
+  function hideTable() {
+    $table.hide();
+  }; 
 
   var $startButton = $("<button>");
   $startButton.html("Let's See!");
   $("#start").append($startButton);
 
-  var questions = {
-      "firstQ": "In what year was Michael Jordan drafted?", 
-      "secondQ": "Who was the previous NBA COmmisioner before Adam Silver?", 
-      "thirdQ": "Which NBA Franchise has won the most NBA Championship Titles?", 
-  }; 
   $startButton.on("click", function() {
-    console.log(questions)
+    console.log(questions);
     console.log("Let's Begin!");
-    alert("Shall We Proceed?");
+    confirm("Shall We Proceed?");
     $startButton.hide();
-    $table.show();
-    var firstQ = "In what year was Michael Jordan drafted into the NBA?";
-    $("#firstQ").text(firstQ);
-
-    
+    showTable();
   });
+
+  function showTable() {
+    $("#firstQ").text(questions.firstQ);
+    $("#secondQ").text(questions.secondQ); 
+    $("#thirdQ").text(questions.thirdQ);
+    $("#fourthQ").text(questions.fourthQ); 
+    $("#fifthQ").text(questions.fifthQ);
+    $("#sixthQ").text(questions.sixthQ); 
+    $("#seventhQ").text(questions.seventhQ);
+    $table.show();
+
+  }; 
 });
--- psuedo.md
+++ psuedo.md
@@ -2,6 +2,32 @@
 HTML
 1. Create all divs. Add references to external sheets
 2. Think about classes and ID's 
-
+3. TIMER DIV
 CSS
 1. 
+
+
+JS 
+1. Establish Questions Vars
+>>From Current Position, next step is to 
+2. Set Timer variables, then timer function. 
+2a.  Timer vars are: number - (seconds starting from); intervalId - (undeclared for now,can name this  variable"timer"); Correct Answers = 0; incorrect count = 0; unanswered = 0;                              <-- TO.BE.CHANGED.ACCORDING.TO.ANSWERS
+3. Set all functions BELOW 
+4. showquestions();
+>>Append All Questions to Correct Div's
+>>Remeber to call showquestions() and countdown() functions at same time 
+>>
+5. Timer();
+>> create timer function timer() {
+>>          timerval = setInterval(decrement, 1000)           
+>>      }     
+>> Start timers. if timer is > 0, *******, else {finished()}
+>>>if timer is >0, 
+5. decrement(); 
+>>number --
+>> append number to header via jquery and strng interpolation
+>>function in which $(.header).html = 
+6. finished(); 
+>>>a. Stop Timer funct (stop) { (clearinterval(timerval)) }
+>>>b. 
+7. 
